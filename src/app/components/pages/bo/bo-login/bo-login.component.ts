7502bfc7d06549cddb4e28d4a918c74e30511150
Ricky <rickyandrianaivo@gmail.com>
2025-02-09 16:36:50 +0300

    bo login page

---
+++ src/app/components/pages/bo/bo-login/bo-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bo-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './bo-login.component.html',
+  styleUrl: './bo-login.component.css'
+})
+export class BoLoginComponent {
+
+}
